cd02bd2516f3e1364398fc29540e3fc4ba2a5f85
Arifa99 <arifanafees99@gmail.com>
2020-04-11 20:11:17 +0530

    Add lerner login/reg page

--- rapid-relief.py
+++ rapid-relief.py
@@ -7,5 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/lerner_reg')
+def lerner_reg():
+    return render_template('lerner_reg.html')
+
+@app.route('/lerner_login')
+def lerner_login():
+    return render_template('lerner_login.html')
+
+@app.route('/lerner_dashboard')
+def lerner_dashboard():
+    return render_template('lerner_dashboard')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
